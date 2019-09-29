e4feac4b739675705ea21fc315d0a6c8755f63d6
Giuliano Cernada <giuliano.cernada@hotmail.com>
2019-09-28 22:27:13 -0300

    Found out the problem. Will develop on it

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 let scale = document.getElementById('scalenumber');
-let scale2 = scale.value;
-console.log(scale2);
 let result = document.getElementById('result-number');
 let measure = document.getElementById('measure-number').value;
 let unit = document.getElementById('selector');
 let calculate = document.getElementById('calc');
 
 function Converted() {
-	document.getElementById('demo').innerHTML = scale2;
+	console.log(scale.value);
 }
 
 // calc.addEventListener('click', Converted)
