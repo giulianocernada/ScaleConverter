4117a3d052b39368e63feebb3a1c3bb0c4cca0d1
Giuliano Cernada <giuliano.cernada@hotmail.com>
2019-09-29 14:10:20 -0300

    Ready. Working properly

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-let scale = document.getElementById('scalenumber');
-let result = document.getElementById('result-number');
-let measure = document.getElementById('measure-number').value;
-let unit = document.getElementById('selector');
-let calculate = document.getElementById('calc');
-
 function Converted() {
-	console.log(scale.value);
+	let scale = document.getElementById('scalenumber').value;
+	let measure = document.getElementById('measure-number').value;
+	let result = document.getElementById('result-number');
+	let unit = document.getElementById('selector').value;
+	let digitos = document.getElementById('redondeo').value;
+	let calcResult = '';
+	if(unit === 'm'){
+		calcResult = ((100/scale) * measure).toFixed(digitos);
+	} else if (unit === 'cm'){
+		calcResult = ((100/scale) * (measure/100)).toFixed(digitos);
+	}
+	result.value = calcResult;
+}
+
+function Reset() {
+	document.getElementById('my-form').reset();
 }
 
 // calc.addEventListener('click', Converted)
+
+//1 en 100
+//1m --- 1cm
+
+//1 en 50
+//1m --- 2cm
